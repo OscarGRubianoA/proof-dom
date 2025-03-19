a2cebed40277df119f2cbfc540f101c50e1b5222
OscarGRubianoA <oscargio86@hotmail.com>
2025-03-19 03:15:37 -0500

    'data'

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let container = document.getElementById("container")
+console.log(data)
+
+let warriors = []
+for (const warrior of data.items) {
+    warriors.push (warrior)
+}
+console.log(warriors)
+
+let createCards =function(objeto){
+   return `<div>
+            <img src="${objeto.image}" alt="">
+            <p>name: ${objeto.name} </p>
+            <p>Ki: ${objeto.ki}</p>
+            <p>MaxKi: ${objeto.maxKi}</p>
+            <p>Description: ${objeto.description}</p>
+
+
+        </div>`
+}
+for (const warrior of warriors) {
+   container.innerHTML+=(createCards(warrior))
+}
+
